f187082bf4df9bd577023f6a578178b3e539c537
Nicolas Schmid <nicolas@breiten.ch>
2021-04-11 08:50:58 +0200

    feat: add scss build pipeline

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,5 +1,4 @@
 import App from './components/App';
-import './style.css';
 
 const btn = document.getElementById('myButton');
 
--- javascript/webpack.config.js
+++ javascript/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -22,15 +21,10 @@ module.exports = {
         extensions: ['*', '.js', '.jsx'],
     },
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, 'js'),
         filename: '[name].js',
     },
-    optimization: {
-        splitChunks: { chunks: 'all' },
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-        }),
-    ],
+    // optimization: {
+    //     splitChunks: { chunks: 'all' },
+    // },
 };
